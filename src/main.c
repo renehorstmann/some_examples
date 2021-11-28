5349bc4fe30fa7d9883f37520165c5c4c855b73e
renehorstmann <r.horsty@gmail.com>
2021-11-28 09:10:08 +0100

    gpush

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@
 #include "camera.h"
 #include "button.h"
 
+#define EXAMPLES 8
+
 static struct {
     eWindow *window;
     eInput *input;
@@ -17,18 +19,21 @@ static struct {
     void (*render_fun)();
 
     Camera_s camera;
+    
+    // example select
+    RoText info;
     RoBatch buttons;
+    RoText button_text[EXAMPLES];
 } L;
 
 
 
-static void on_pointer_callback(ePointer_s pointer, void *ud) {
-   
-}
 
 
 static void main_loop(float delta_time);
 
+static void example_select_init();
+static void example_select_kill();
 static void example_select_update(float dtime);
 static void example_select_render();
 
@@ -53,11 +58,8 @@ int main(int argc, char **argv) {
 
     // init systems
     L.camera = camera_new();
-
-    // setup a pointer listener
-    e_input_register_pointer_event(L.input, on_pointer_callback, NULL);
     
-    L.buttons = ro_batch_new(1, r_texture_new_white_pixel());
+    example_select_init();
     
     
     L.update_fun = example_select_update;
@@ -101,19 +103,77 @@ static void main_loop(float delta_time) {
     r_render_end_frame(L.render);
 }
 
+
+static void example_select_pointer_callback(ePointer_s pointer, void *ud) {
+   pointer.pos = mat4_mul_vec(L.camera.matrices.v_p_inv, pointer.pos);
+   
+   for(int i=0; i<EXAMPLES; i++) {
+       if(button_clicked(&L.buttons.rects[i], pointer)) {
+           
+       }
+   }
+}
+
+
+static void example_select_init() {
+    // setup a pointer listener
+    e_input_register_pointer_event(L.input, example_select_pointer_callback, NULL);
+    
+    L.info = ro_text_new_font55(32);
+    ro_text_set_text(&L.info, "some examples");
+    
+    L.buttons = ro_batch_new(EXAMPLES, r_texture_new_file(2, 1, "res/big_btn.png"));
+    
+    for(int i=0; i<EXAMPLES; i++) {
+        L.button_text[i] = ro_text_new_font85(32);
+        char text[32];
+        snprintf(text, 32, "Example: %i", i);
+        ro_text_set_text(&L.button_text[i], text);
+        ro_text_set_color(&L.button_text[i], R_COLOR_BLACK);
+    }
+}
+
+static void example_select_kill() {
+    e_input_unregister_pointer_event(L.input, example_select_pointer_callback);
+    
+    ro_text_kill(&L.info);
+    ro_batch_kill(&L.buttons);
+    for(int i=0; i<EXAMPLES; i++) {
+        ro_text_kill(&L.button_text[i]);
+    }
+}
+
 static void example_select_update(float dtime) {
     ivec2 window_size = e_window_get_size(L.window);
 
     // simulate
     camera_update(&L.camera, window_size.x, window_size.y);
     
+    // text position is (unlike most render objects) in the top left (instead of the centre)
+    u_pose_set_xy(&L.info.pose, -48, 90);
+    
+    for(int i=0; i<EXAMPLES; i++) {
+        float y = 70-18*i;
+        L.buttons.rects[i].pose = u_pose_new(0, y, 128, 16);
+        
+        u_pose_set_xy(&L.button_text[i].pose, 
+                32-128/2,
+                y + (button_is_pressed(&L.buttons.rects[i])? 4 : 6));
+    }
+    
+    // push batch changes into the gpu memory
+    ro_batch_update(&L.buttons);
 }
 
 static void example_select_render() {
-     mat4 *cam_mat = &L.camera.matrices.vp;
+    mat4 *cam_mat = &L.camera.matrices.vp;
+     
+    ro_text_render(&L.info, cam_mat);
      
      // buttons
     ro_batch_render(&L.buttons, cam_mat);
-
+    for(int i=0; i<EXAMPLES; i++) {
+        ro_text_render(&L.button_text[i], cam_mat);
+    }
 }
 
